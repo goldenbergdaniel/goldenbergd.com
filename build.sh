d7b09fb08e50f8394b20e08a4000874b8753cbd1
Daniel Goldenberg <goldenbergdan@proton.me>
2025-04-29 02:25:12 -0400

    Improve build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 rm -rf dist
+
+if [[ $1 == "clean" ]]; then exit 0; fi;
+
+rm -rf dist
 mkdir dist
-cp -r pages/* dist
-cp -r public dist
+cp -r pages/* public dist
 
 bin/esbuild scripts/main.js --bundle --outfile=dist/scripts/main.js --minify --target=es6
 
 if [[ $1 == "run" ]]; then
-  busybox httpd -f -p 8080 -h dist
+  busybox httpd -h dist -p 8080 -f
 fi;
