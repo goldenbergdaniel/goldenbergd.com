e3adf48bfbfbac1f1e31b744c8aca674cd554d23
Daniel Goldenberg <goldenbergdan@proton.me>
2025-05-15 13:57:39 -0400

    Add footer and improve style

--- build.sh
+++ build.sh
@@ -5,9 +5,9 @@ rm -rf dist
 if [[ $1 == "clean" ]]; then exit 0; fi;
 
 mkdir dist
-cp -r pages/* assets scripts dist
+cp -r pages/* assets dist
 
 if [[ $1 == "run" ]]; then
-  echo "Server running on http://localhost:8080/"
+  echo "Server running on http://127.0.0.1:8080/"
   busybox httpd -h dist -p 8080 -f
 fi;
